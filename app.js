a44841f6f725255a1823355eca81ced27968d04b
root <root@precise64.(none)>
2014-10-17 12:14:01 +0000

    changed to check build fail

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello, we are testing continous integration using Jenkins via github.');
+  res.send('hello, we are testing continous integration using Jenkins via github');
 });
  
 app.listen(process.env.PORT || 5000);
